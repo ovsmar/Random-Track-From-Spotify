978ffc57b656824c539362d3d7d97996cf587107
ovsmar <ovsep.simonian@gmail.com>
2023-04-30 19:12:12 +0200

    Random track from spotify

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const CLIENT_ID = "94597b959a054486b5d1cee164c4f796";
 const CLIENT_SECRET = "f6ac005922a8450e961c2732c0f87e70";
 const AUTH_URL = "https://accounts.spotify.com/api/token";
-const PLAYLIST_ID = "1mB0DBalm5cTnkoi8yDFul";
-const TRACKS_URL = `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}/tracks`;
+const SEARCH_URL = "https://api.spotify.com/v1/search";
+const TYPE = "track";
 
 let auth_token;
+let previousTracks = [];
 
 async function getAuthToken() {
   const response = await fetch(AUTH_URL, {
@@ -18,18 +19,26 @@ async function getAuthToken() {
   const data = await response.json();
   auth_token = data.access_token;
 }
+
 getAuthToken();
 
-async function getRandomTrack() {
-  const response = await fetch(TRACKS_URL, {
+async function getRandomTrack(year) {
+  const query = encodeURIComponent(`year:${year}`); // modify this query to search for the tracks you want
+  const url = `${SEARCH_URL}?q=${query}&type=${TYPE}&limit=50`;
+  const response = await fetch(url, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${auth_token}`,
     },
   });
   const data = await response.json();
-  const tracks = data.items;
+  const tracks = data.tracks.items.filter(track => !previousTracks.includes(track.id));
+  if (tracks.length === 0) {
+    previousTracks = [];
+    return getRandomTrack();
+  }
   const randomTrack = tracks[Math.floor(Math.random() * tracks.length)];
+  previousTracks.push(randomTrack.id);
   return randomTrack;
 }
 
@@ -38,8 +47,9 @@ const trackEl = document.querySelector(".track");
 const audioEl = document.querySelector("#audio");
 
 btn.addEventListener("click", async () => {
-  const track = await getRandomTrack();
-  const { name, preview_url, external_urls } = track.track;
+  const year = document.querySelector("#year").value;
+  const track = await getRandomTrack(year);
+  const { name, preview_url, external_urls } = track;
   if (preview_url) {
     trackEl.innerHTML = `Track Name: <a href="${external_urls.spotify}" target="_blank">${name}</a>`;
     audioEl.src = preview_url;
@@ -52,10 +62,25 @@ btn.addEventListener("click", async () => {
     trackEl.style.borderRadius = "4px";
     trackEl.style.marginBottom = "20px";
 
-    const albumCoverUrl = track.track.album.images[0].url;
+    const albumCoverUrl = track.album.images[0].url;
     document.getElementById("bg-image").src = albumCoverUrl;
   } else {
-    trackEl.innerHTML = `Sorry, this track <a href="${track.track.external_urls.spotify}" target="_blank">${track.track.name}</a> does not have a preview available, please click again!`;
+    trackEl.innerHTML = `Sorry, this track <a href="${external_urls.spotify}" target="_blank">${name}</a> does not have a preview available, please click again!`;
     trackEl.style.backgroundColor = "red";
   }
 });
+
+
+const select = document.querySelector("#year");
+
+// Loop from 1920 to 2023 and create an option for each year
+for (let i = 1900; i <= 2023; i++) {
+  const option = document.createElement("option");
+  option.value = i;
+  option.text = i;
+  if (i === 2023) {
+    option.selected = true;
+  }
+  select.appendChild(option);
+}
+
